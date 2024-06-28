a5493cc64fda1f3314da74e82a1dfe8c0169b6c1
DevFranceca <dynamicfranchescer@gmail.com>
2024-06-28 16:18:26 +0100

    finish my task component styles

--- src/components/tasks/page.js
+++ src/components/tasks/page.js
@@ -4,7 +4,23 @@ import styles from "./tasks.module.css"
 const Tasks = () => {
   return (
     <div className={styles.tasksCon}>
-      Tasks
+      <div className={styles.tasksWrapper}>
+        <section className={styles.iconText}>
+            <img src="" alt='icon'/>
+            <h2 className={styles.chesaTodo}>ChesaTodo</h2>
+        </section>
+        <section className={styles.myListsCon}>
+            <h4 className={styles.myLists}>My Lists</h4>
+            <div className={styles.listContainer}>
+                <article className={styles.taskList}>
+                    <p>Life</p>
+                    <div className={styles.middleLine}></div>
+                    <img src="" alt='x'/>
+                </article>
+                <p className={styles.newList}>+ New List</p>
+            </div>
+        </section>
+      </div>
     </div>
   )
 }
