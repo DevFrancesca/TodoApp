4da19dc2cf91267f58f4261f87fc92794e3cece4
DevFranceca <dynamicfranchescer@gmail.com>
2024-07-08 17:54:02 +0100

    made new changes on my component

--- src/app/todoApp/page.js
+++ src/app/todoApp/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import styles from "./todoApp.module.css"
 import Calendar from '@/components/calender/page'
--- src/components/tasks/page.js
+++ src/components/tasks/page.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import styles from "./tasks.module.css"
+import { useState } from 'react'
 
 const Tasks = () => {
+  const [isActive, setIsActive] = useState(false)
+
+  const toggleActive = ()=>{
+    setIsActive(!isActive)
+  }
   return (
     <div className={styles.tasksCon}>
       <div className={styles.tasksWrapper}>
@@ -12,11 +18,12 @@ const Tasks = () => {
         <section className={styles.myListsCon}>
             <h4 className={styles.myLists}>My Lists</h4>
             <div className={styles.listContainer}>
-                <article className={styles.taskList}>
+                <article className={styles.active}>
                     <p>Life</p>
                     <div className={styles.middleLine}></div>
-                    <img src="" alt='x'/>
+                    <img src="./cancel.svg" alt='x'/>
                 </article>
+                <p className={styles.inactive}>Work</p>
                 <p className={styles.newList}>+ New List</p>
             </div>
         </section>
