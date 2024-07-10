35818f549cf8648e0dfdd429774713ed08863eab
DevFranceca <dynamicfranchescer@gmail.com>
2024-07-10 11:48:57 +0100

    started on my todo component

--- src/components/tasks/page.js
+++ src/components/tasks/page.js
@@ -1,42 +1,38 @@
-import React, { useState } from 'react';
-import styles from './tasks.module.css';
+import React from 'react'
+import styles from "./tasks.module.css"
+import { useState } from 'react'
 
 const Tasks = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false)
 
-  const toggleActive = () => {
-    setIsActive(!isActive);
-  };
-
+  const toggleActive = ()=>{
+    setIsActive(!isActive)
+  }
   return (
     <div className={styles.tasksCon}>
       <div className={styles.tasksWrapper}>
         <section className={styles.iconText}>
-          <img src="./lightening.svg" alt="icon" />
-          <h2 className={styles.chesaTodo}>ChesaTodo</h2>
+            <img src="./lightening.svg" alt='icon'/>
+            <h2 className={styles.chesaTodo}>ChesaTodo</h2>
         </section>
         <section className={styles.myListsCon}>
-          <h4 className={styles.myLists}>My Lists</h4>
-          <div className={styles.listContainer}>
-            {isActive ? (
-              <article className={styles.active} onClick={toggleActive}>
-                <p>Life</p>
-                <div className={styles.middleLine}></div>
-                <img src="./cancel.svg" alt="x" />
-              </article>
-            ) : (
-              <article className={styles.inactive} onClick={toggleActive}>
-                <p>Work</p>
-                <p>Work</p>
-                <p>Work</p>
-              </article>
-            )}
-            <p className={styles.newList}>+ New List</p>
-          </div>
+            <h4 className={styles.myLists}>My Lists</h4>
+            <div className={styles.listContainer}>
+                {
+                  isActive? <article className={styles.active} onClick={toggleActive}>
+                              <p>Life</p>
+                              <div className={styles.middleLine}></div>
+                              <img src="./cancel.svg" alt='x'/>
+                            </article>
+                             :
+                        <p className={styles.inactive} onClick={toggleActive}>Work</p>
+                }
+                <p className={styles.newList}>+ New List</p>
+            </div>
         </section>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Tasks;
+export default Tasks
--- src/components/todoList/page.js
+++ src/components/todoList/page.js
@@ -4,7 +4,19 @@ import styles from "./todoList.module.css"
 const TodoList = () => {
   return (
     <div className={styles.todoListCon}>
-      TodoList
+      <section className={styles.todoWrapper}>
+        <article className={styles.todoTop}>
+          <div className={styles.todoTopLeft}>
+            <h4 className={styles.month}>Feb</h4>
+            <h4 className={styles.day}>4</h4>
+          </div>
+          <div className={styles.todoTopRight}>
+            <h2 className={styles.greeting}>Good Afternoon.</h2>
+            <h3 className={styles.question}>What's your plan for today?</h3>
+          </div>
+        </article>
+        <div className={styles.todoBottom}></div>
+      </section>
     </div>
   )
 }
